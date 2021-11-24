9c758f5d248c36de7bdf116b6da56a2be0e9c85a
francois <francois.lorne@student-cs.fr>
2021-11-24 20:02:59 +0100

    Prepare for db debug The MySQL db is not available, so this is only the groundwork

--- backend/src/controllers/data.ts
+++ backend/src/controllers/data.ts
@@ -1,25 +1,15 @@
 import * as express from "express";
 
+import { mysqlConnection } from "./../service/mysqlConnection";
+
 export const dataController = express.Router();
 
 dataController.get('/all', (_, res) => {
-  res.status(200).json({
-    data: 'This is all the data'
-  });
+  mysqlConnection( (err, dbResponse) => {
+    if (!err) {
+      res.status(200).json(dbResponse);
+    } else {
+      res.status(201).json(err);
+    }
+  })
 });
-
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'birdie-test.cyosireearno.eu-west-2.rds.amazonaws.com',
-  user     : 'dbuser',
-  password : 'xnxPp6QfZbCYkY8'
-});
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
--- config.tsx
+++ config.tsx
@@ -1,7 +1,5 @@
-const BACK_TOKEN = 'DzpegmwSOuQUzsaQEZqmvCHr9aPXnXX8kAuUshOmzX4rSgW4EisKXjz6Ykng5gUCTLQlDntUP6kJaeqK8dmnG3OSbOzQLhePcAjrXkvZzAiwLQnr6qbZLPuKfbuJD9Gx'
 const BACK_URL = 'http://http://localhost:8000'
 
 export default {
-  BACK_TOKEN,
   BACK_URL
 }
