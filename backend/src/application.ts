8af0cb4e23491ae1b7de427223e7f56f4e1a8cf5
francois <francois.lorne@student-cs.fr>
2021-11-28 23:41:09 +0100

    Updating according to deployment errors

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,13 +1,10 @@
 import * as express from "express";
 import {pingController} from "./controllers/ping";
-import {dataController} from "./controllers/data.router"; 
+import {dataController} from "./controllers/data.router";
+import cors from 'cors';
 
 const app = express();
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://birdie-test-lorne.herokuapp.com/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
   
 app.use('/hello', pingController);
 app.use('/data', dataController);
--- frontend/src/services/getPatientEvents.tsx
+++ frontend/src/services/getPatientEvents.tsx
@@ -5,22 +5,24 @@ export default async function getPatientEvents(id: string, setData: Function) {
   const route: string =
     id !== '404'
       ? `https://birdie-test-lorne-back.herokuapp.com/data/${id}`
-      : 'https://birdie-test-lorne-back.herokuapp.com/data/all';
+      : 'https://birdie-test-lorne-back.herokuapp.com/hello';
 
-  const response = await axios.get(route).catch(err => {
-    console.log('Server error: ', err);
-  });
-  if (response) {
-    const resData = response.data;
-    console.log(`Successful response from backend at ${route}`);
-    if (response.status === 200) {
-      setData(resData);
-      console.log(`patient changed to ${id} and useEffect called`);
-    } else {
-      console.log(`Error from ${route}: `, resData.err);
-      setData(resData.err);
-    }
-  } else {
-    console.log('Server error');
-  }
+  await axios
+    .get(route)
+    .then(response => {
+      if (response) {
+        const resData = response.data;
+        console.log(`Successful response from backend at ${route}`);
+        if (response.status === 200 && typeof resData === 'object') {
+          setData(resData);
+          console.log(`patient changed to ${id} and useEffect called`);
+        } else {
+          console.log(`Error from ${route}: `, resData.err);
+          setData(resData.err);
+        }
+      }
+    })
+    .catch(err => {
+      console.log('Server error: ', err);
+    });
 }
